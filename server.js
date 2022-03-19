b70ea57e3dd34f36f2ad27f9152866f9ff2012e8
Dan Tiberi <danny.tiberi@gmail.com>
2022-03-19 13:45:58 -0500

    DB Connection Implemented

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+'use strict';
 require('dotenv').config();
-var express = require('express');;
+var express = require('express');
 
 console.log("Starting server...");
 //console.log(process.env.APPLICATION_SQL_USERNAME);
 
 var app = express();
+
+//DB Connection
+const DbConn = require('./objects/dbConn');
+var serverShutdown = require('./objects/serverShutdown')
+const dbConn = require('./objects/dbConn');
+
+exports.dbConn = dbConn;
+
+serverShutdown.setDbConn(dbConn);
+dbConn.openConnection();
+//End
 
 app.get('/', function (req, res) { //Default api call
     res.send("Hello world!");
