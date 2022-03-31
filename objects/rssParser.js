15eebdbf128e9a8b8f938e38bb21009a0a90693c
Dan Tiberi <danny.tiberi@gmail.com>
2022-03-31 08:02:13 -0500

    Function for getting string from RSS feed data.

--- objects/rssParser.js
+++ objects/rssParser.js
@@ -21,7 +21,29 @@ class RssParser {
         })();
     }
 
-    
+    async getDataString(url, callback) {
+        let parser = new Parser();
+
+        (async () => {
+            let feed = await parser.parseURL(url);
+            
+            var output = feed.title + " ";
+            
+            feed.items.forEach(item => {
+                //console.log(item.title + ':' + item.link);
+                output = output + item.title;
+            });
+
+            /*
+            console.log(feed.title);
+            feed.items.forEach(item => {
+                console.log(item.title + ':' + item.link)
+            });
+            */
+
+            callback(output);
+        })();
+    }
 
     constructor() {
         //console.log("Parser created.");
