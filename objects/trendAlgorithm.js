e3db6ff24eada79416f2bd26b02790adbfa4ffb7
Dan Tiberi <danny.tiberi@gmail.com>
2022-03-30 21:03:46 -0500

    Print hashtable function.

--- objects/trendAlgorithm.js
+++ objects/trendAlgorithm.js
@@ -2,23 +2,25 @@
 var SimpleHashTable = require('simple-hashtable');
 //https://www.npmjs.com/package/simple-hashtable
 class TrendAlgorithm {    
-    constructor(data){
-        this.data = data;
-        this.hashtable = new SimpleHashTable();
-        this.eachWord(data);
+
+    printHashtable(hashtable){
+        var keys = hashtable.keys();
+        for(let i = 0; i < keys.length; i++){
+            console.log(`Key: ${keys[i]} Value: ${hashtable.get(keys[i])}`);
+        }
     }
 
     /*
     Adds the given word to the hashtable with count 1; If it's already there it will increase the count by one.
     */
-    addToHashtable(word){
-        if(this.hashtable.containsKey(word)){
-            const oldCount = this.hashtable.get(word);
-            this.hashtable.remove(word);
-            this.hashtable.put(word, oldCount+1);
+    addToHashtable(word, hashtable){
+        if(hashtable.containsKey(word)){
+            var oldCount = hashtable.get(word);
+            hashtable.remove(word);
+            hashtable.put(word, oldCount+1);
         }
         else {
-            this.hashtable.put(word, 1);
+            hashtable.put(word, 1);
         }
     }
 
@@ -27,13 +29,21 @@ class TrendAlgorithm {
     data: string to iterate through.
     callback: function to call with each individual word as a parameter.
     */
-    eachWord(data, callback) {
+    eachWord(data, callback, hashtable) {
         var words = data.split(" ");
 
         for(let i = 0; i < words.length; i++){
             //console.log(words[i]);
+            callback(words[i], hashtable);
         }
     }   
+
+    constructor(data){
+        this.data = data;
+        this.hashtable = new SimpleHashTable();
+        this.eachWord(data, this.addToHashtable, this.hashtable);
+        this.printHashtable(this.hashtable);
+    }
 }
 
 module.exports = TrendAlgorithm;
