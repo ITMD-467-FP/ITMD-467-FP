c7bd9656d9385732b34b69bee40738a384e51d83
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-11 13:38:22 -0500

    Better description for insert test.

--- test/trendAlgorithmTest.js
+++ test/trendAlgorithmTest.js
@@ -5,10 +5,10 @@ const assert = require("assert");
 const TrendAlgorithm = require('../objects/trendAlgorithm');
 
 //https://blog.logrocket.com/a-quick-and-complete-guide-to-mocha-testing-d0e0ea09f09d/
-describe("Trend Algorithm Function Tests", function () {
+describe("Trend Algorithm Tests", function () {
     const alg = new TrendAlgorithm();
     alg.insert("Hi Dan how are you doing today?", "null", false);
-    it("Should contain a hash table with 7 keys.", function (done) {
+    it("Should contain a hash table with 7 keys given input: 'Hi Dan how are you doing today?'", function (done) {
         assert.equal(alg.hashtable.keys().length, 7);
         done();
     });
