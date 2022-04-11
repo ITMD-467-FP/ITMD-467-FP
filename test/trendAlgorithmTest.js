1b644fe9cd88747fb74bb83e89d6d012237b361c
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-11 14:05:04 -0500

    Filter test. Lowercase Test

--- test/trendAlgorithmTest.js
+++ test/trendAlgorithmTest.js
@@ -15,14 +15,45 @@ describe("Trend Algorithm Insert Tests. Input: 'Hi Dan how are you doing today?"
     });
 
     it("Should contain a single source for each term.", function (done) {
-        assert.equal(alg.hashtable.get("Dan")[1].length, 1);
+        assert.equal(alg.hashtable.get("dan")[1].length, 1);
         done();
     });
 
     it("Should contain multiple sources when input repeated with a different source.", function (done) {
         alg.insert("Hi Dan how are you doing today?", "The Other News", false);
 
-        assert.equal(alg.hashtable.get("Dan")[1].length, 2);
+        assert.equal(alg.hashtable.get("dan")[1].length, 2);
+
+        // Cleanup. Undo insert.
+        alg = new TrendAlgorithm();
+        alg.insert("Hi Dan how are you doing today?", "The News", false);
+
+        done();
+    });
+
+    it("Should filter out commonly used words from filter.json.", function (done) {
+
+        alg.insert("The is AnD Airplane!", "The Other News", true);
+
+        chai.assert.equal(alg.hashtable.get("the"), -1);
+        chai.assert.equal(alg.hashtable.get("is"), -1);
+        chai.assert.equal(alg.hashtable.get("and"), -1);
+        chai.assert.equal(alg.hashtable.get("airplane!")[0], 1);
+
+        // Cleanup. Undo insert.
+        alg = new TrendAlgorithm();
+        alg.insert("Hi Dan how are you doing today?", "The News", false);
+
+        done();
+    });
+
+    it("Should convert all inputs to lowercase", function (done) {
+
+        alg.insert("OHHH WOW thE!", "The Other News", false);
+
+        chai.assert.equal(alg.hashtable.get("the!")[0], 1);
+        chai.assert.equal(alg.hashtable.get("wow")[0], 1);
+        chai.assert.equal(alg.hashtable.get("ohhh")[0], 1);
 
         // Cleanup. Undo insert.
         alg = new TrendAlgorithm();
