31dd5a5eee4e4b7d4d4776b198de47c1578d6191
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-11 13:37:16 -0500

    Updated to match new trend algorithm changes.

--- test/trendAlgorithmTest.js
+++ test/trendAlgorithmTest.js
@@ -6,7 +6,8 @@ const TrendAlgorithm = require('../objects/trendAlgorithm');
 
 //https://blog.logrocket.com/a-quick-and-complete-guide-to-mocha-testing-d0e0ea09f09d/
 describe("Trend Algorithm Function Tests", function () {
-    const alg = new TrendAlgorithm("Hi Dan how are you doing today?");
+    const alg = new TrendAlgorithm();
+    alg.insert("Hi Dan how are you doing today?", "null", false);
     it("Should contain a hash table with 7 keys.", function (done) {
         assert.equal(alg.hashtable.keys().length, 7);
         done();
