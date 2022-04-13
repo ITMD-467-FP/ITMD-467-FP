c7e630b611bfbced13f834018e95b48bfd482761
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-13 16:38:33 -0500

    getPassword function.

--- routes/userLogin.js
+++ routes/userLogin.js
@@ -1,7 +1,6 @@
 module.exports = function (app) { //receiving "app" instance
     app.route('/userLogin')
         .get(getAPI)
-        .post(postAPI);
 }
 
 
@@ -10,6 +9,7 @@ var {
 } = require('../server.js');
 const sql = require('mssql')
 var passwordHash = require('password-hash');
+const RssParser = require('../objects/rssParser.js');
 
 /*
 Description:
@@ -29,7 +29,40 @@ function getUser() {
     
 }
 
+async function getPassword(email) {
+    return new Promise((resolve, reject) => {
+        const command = "SELECT TOP(1) password FROM app_user WHERE email = @email;" //TOP(1) limits to one row.
 
+        dbConn.openConnection().then((pool) => {
+            const ps = new sql.PreparedStatement(pool);
+            ps.input('email', sql.VarChar)
+        
+            ps.prepare(command, err => {
+                if (err) {
+                    console.log(err);
+                }
+        
+                ps.execute({
+                    email: email,
+                }, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        //console.log(result.recordset[0].password);
+                        resolve(result.recordset[0].password);
+                    }
+        
+                    ps.unprepare(err => {
+                        if (err) {
+                            reject(err);
+                            console.log(err);
+                        }
+                    });
+                });
+            }); 
+        });
+    });
+}
 
 function verifyPassword(input, storedHash) {
     return passwordHash.verify(input, storedHash);
@@ -38,6 +71,9 @@ function verifyPassword(input, storedHash) {
 function getAPI(req, res) {
     const email = req.body.email;
     const password = passwordHash.generate(req.body.password);
-
-
+    
+    (async () => {
+        //res.status(200);
+        //res.send(await getPassword(email));
+    })();
 }
