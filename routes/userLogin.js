d4521a323c100fda1c3b69bfe1db21e9a1a6e49a
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-13 16:49:08 -0500

    Login verification and return.

--- routes/userLogin.js
+++ routes/userLogin.js
@@ -25,8 +25,39 @@ Parameters:
 
 //https://www.npmjs.com/package/password-hash
 
-function getUser() {
-    
+async function getUser(email) {
+    return new Promise((resolve, reject) => {
+        const command = "SELECT TOP(1) * FROM app_user WHERE email = @email;" //TOP(1) limits to one row.
+
+        dbConn.openConnection().then((pool) => {
+            const ps = new sql.PreparedStatement(pool);
+            ps.input('email', sql.VarChar)
+        
+            ps.prepare(command, err => {
+                if (err) {
+                    console.log(err);
+                }
+        
+                ps.execute({
+                    email: email,
+                }, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        //console.log(result.recordset[0]);
+                        resolve(result.recordset[0]);
+                    }
+        
+                    ps.unprepare(err => {
+                        if (err) {
+                            reject(err);
+                            console.log(err);
+                        }
+                    });
+                });
+            }); 
+        });
+    });
 }
 
 async function getPassword(email) {
@@ -70,9 +101,30 @@ function verifyPassword(input, storedHash) {
 
 function getAPI(req, res) {
     const email = req.body.email;
-    const password = passwordHash.generate(req.body.password);
+    const password = req.body.password;
     
+    console.log(passwordHash.generate("hunter123"));
+
     (async () => {
+
+        /*
+        Steps:
+        - Get password from db
+        - Verify
+        - If valid return getUser(email)
+        */
+    
+        getPassword(email).then((storedPassword) => {
+            res.status(200);
+            if(verifyPassword(password, storedPassword)){
+                getUser(email).then((userRow) => {
+                    res.send(userRow);
+                })
+            }
+            else {
+                res.send("{'error': 'Invalid username or password.'}");
+            }
+        })
         //res.status(200);
         //res.send(await getPassword(email));
     })();
