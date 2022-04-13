bb70fb0b8d27318eb0b65144a0d638dd802fd540
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-13 17:15:53 -0500

    Insert source sql command.

--- routes/addSource.js
+++ routes/addSource.js
@@ -22,6 +22,36 @@ Parameters:
 
 //https://www.npmjs.com/package/password-hash
 
+exports.insertSource = async function insertSource(source, userId) {
+    return new Promise((resolve, reject) => {
+        const command = `
+        DECLARE @NewSourceId TABLE (id INT)
+
+        IF EXISTS (
+            SELECT source.id FROM source
+            INNER JOIN user_source
+            ON user_source.user_id = @userId
+        )
+        BEGIN
+            SELECT source.id, source.url FROM source
+            INNER JOIN user_source
+            ON user_source.id = @userId AND user_source.source_id = source.id
+        END
+        ELSE
+        BEGIN
+            INSERT INTO source (url) 
+            OUTPUT INSERTED.id INTO @NewSourceId(id)
+            VALUES(@source);
+
+            INSERT INTO user_source(user_id, source_id)
+            VALUES (@userId, (SELECT TOP(1) id FROM @NewSourceId));
+        END
+        `;
+
+       
+    });
+}
+
 
 function postAPI(req, res) {
     const userId = req.body.userId;
