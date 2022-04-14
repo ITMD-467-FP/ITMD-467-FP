ed966cc4f16449b102b72ca0c11b627073978903
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-14 15:57:31 -0500

    Copied over and adjusted some useful db query functions for reuse.

---
+++ objects/usefulDBQueries.js
@@ -0,0 +1,78 @@
+var {
+    dbConn
+} = require('../server.js');
+const sql = require('mssql')
+
+exports.getUserByEmail = async (email) => {
+    return new Promise((resolve, reject) => {
+        const command = `
+        SELECT TOP(1) * FROM app_user WHERE email = @email;
+        ` //TOP(1) limits to one row.
+
+        dbConn.openConnection().then((pool) => {
+            const ps = new sql.PreparedStatement(pool);
+            ps.input('email', sql.VarChar)
+        
+            ps.prepare(command, err => {
+                if (err) {
+                    console.log(err);
+                }
+        
+                ps.execute({
+                    email: email,
+                }, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        //console.log(result.recordset[0]);
+                        resolve(result.recordset[0]);
+                    }
+        
+                    ps.unprepare(err => {
+                        if (err) {
+                            reject(err);
+                            console.log(err);
+                        }
+                    });
+                });
+            }); 
+        });
+    });
+}
+
+exports.getUserById = async (id) => {
+    return new Promise((resolve, reject) => {
+        const command = `
+        SELECT TOP(1) * FROM app_user WHERE id = @id;
+        ` //TOP(1) limits to one row.
+
+        dbConn.openConnection().then((pool) => {
+            const ps = new sql.PreparedStatement(pool);
+            ps.input('id', sql.VarChar)
+        
+            ps.prepare(command, err => {
+                if (err) {
+                    console.log(err);
+                }
+        
+                ps.execute({
+                    id: id,
+                }, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        //console.log(result.recordset[0]);
+                        resolve(result.recordset[0]);
+                    }
+        
+                    ps.unprepare(err => {
+                        if (err) {
+                            reject(err);
+                            console.log(err);
+                        }
+                    });
+                });
+            }); 
+        });
+    });
+}
