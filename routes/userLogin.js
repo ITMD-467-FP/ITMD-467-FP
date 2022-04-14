0f84ac69b7cf294744c8c52047183a2fc048d582
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-14 15:32:37 -0500

    Configured to generate session token.

--- routes/userLogin.js
+++ routes/userLogin.js
@@ -25,13 +25,23 @@ Parameters:
 
 //https://www.npmjs.com/package/password-hash
 
-async function getUser(email) {
+var passwordHash = require('password-hash');
+function generateCurrentToken(email, password){
+    return passwordHash.generate(email + password);
+}
+
+async function loginAndGetUser(email, newCurrentToken) {
     return new Promise((resolve, reject) => {
-        const command = "SELECT TOP(1) * FROM app_user WHERE email = @email;" //TOP(1) limits to one row.
+        const command = `
+        UPDATE app_user SET current_secret_token = @newCurrentToken WHERE email = @email;
+
+        SELECT TOP(1) * FROM app_user WHERE email = @email;
+        ` //TOP(1) limits to one row.
 
         dbConn.openConnection().then((pool) => {
             const ps = new sql.PreparedStatement(pool);
             ps.input('email', sql.VarChar)
+            ps.input('newCurrentToken', sql.VarChar)
         
             ps.prepare(command, err => {
                 if (err) {
@@ -40,6 +50,7 @@ async function getUser(email) {
         
                 ps.execute({
                     email: email,
+                    newCurrentToken: newCurrentToken
                 }, (err, result) => {
                     if (err) {
                         console.log(err);
@@ -109,13 +120,13 @@ function getAPI(req, res) {
         Steps:
         - Get password from db
         - Verify
-        - If valid return getUser(email)
+        - If valid return loginAndGetUser(email)
         */
     
         getPassword(email).then((storedPassword) => {
             res.status(200);
             if(verifyPassword(password, storedPassword)){
-                getUser(email).then((userRow) => {
+                loginAndGetUser(email, generateCurrentToken(email, password)).then((userRow) => {
                     res.send(userRow);
                 })
             }
