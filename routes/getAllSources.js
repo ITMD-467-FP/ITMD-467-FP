79ab3b1529a49b3d5239337d600ddf0daf25c497
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-14 20:02:37 -0500

    Integrated with database query.

--- routes/getAllSources.js
+++ routes/getAllSources.js
@@ -17,17 +17,24 @@ Method: get
 Parameters: 
 - In Body:
     - userId: id of the user to query against.
-
 */
 
 //Runs after the token authorization was successful.
 function authComplete(req, res){
-    
-    
+    const userId = req.body.userId;
+
+    (async () => {
+        UsefulDBQueries.getAllSources(userId, dbConn).then(
+            (queryRes) => {
+                res.status(200);
+                res.send(queryRes);
+            }
+        );
+    })();
 }
 
 function getAPI(req, res){
-    authorization.validateToken(req, res, authComplete);
+    authorization.validateToken(req, res, authComplete, false);
 }
 
 function postAPI(req, res){} //This needs to be here. Don't ask me.
