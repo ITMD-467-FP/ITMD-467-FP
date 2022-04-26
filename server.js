c66629c1998d5b324a06f30d704efbb652631a33
Dan Tiberi <danny.tiberi@gmail.com>
2022-04-26 17:57:25 -0500

    Added cors header to responses.

--- server.js
+++ server.js
@@ -56,6 +56,11 @@ app.use(express.json());
 try {
     var cors = require('cors');
     app.use(cors());
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 } catch (error) {
     console.log(error);
 }
